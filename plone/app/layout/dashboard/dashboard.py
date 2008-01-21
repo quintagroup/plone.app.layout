7b1925bb4be2a6f8e923a52894792377dc21a65c
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2008-01-21 13:36:08 +0000

    Added column information and making the column rendering flexible
    svn path=/plone.app.layout/branches/zest-dashboard-dnd/; revision=19055

--- plone/app/layout/dashboard/dashboard.py
+++ plone/app/layout/dashboard/dashboard.py
@@ -3,25 +3,30 @@ from zope.component import getUtility
 from Products.Five.browser import BrowserView
 
 from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IColumnManager
 from plone.portlets.constants import USER_CATEGORY
 
 from plone.memoize.instance import memoize
 
 from Products.CMFCore.utils import getToolByName
 
+
 class DashboardView(BrowserView):
     """Power the dasbhard
     """
-    
+
     @memoize
     def empty(self):
         dashboards = [getUtility(IPortletManager, name=name) for name in
-                        ['plone.dashboard1', 'plone.dashboard2', 'plone.dashboard3', 'plone.dashboard4']]
-                        
+                        getUtility(IColumnManager, 'plone.dashboard')]
+
         portal_membership = getToolByName(self.context, 'portal_membership')
         userid = portal_membership.getAuthenticatedMember().getId()
-                        
+
         num_portlets = 0
         for dashboard in dashboards:
             num_portlets += len(dashboard.get(USER_CATEGORY, {}).get(userid, {}))
         return num_portlets == 0
+    
+    def list_columns(self):
+        return getUtility(IColumnManager, 'plone.dashboard').list_columns()
